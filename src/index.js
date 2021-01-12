05eedf568198208b703c1112162255b2d5ffe870
zg-zhang <493174665@qq.com>
2021-01-12 17:57:19 +0800

    步骤一：createElement 函数

--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom'
 
-const element = <div>这是一个div元素</div>
+function createElement(type, props, ...children) {
+	return {
+		type,
+		props: {
+			...props,
+			children: children.map(child =>
+				typeof child === "object"
+					? child
+					: createTextElement(child)
+			)
+		}
+	}
+}
 
-ReactDOM.render(element, document.getElementById('root'))
+function createTextElement(text) {
+	return {
+		type: 'TEXT_ELEMENT',
+		props: {
+			nodeValue: text,
+			children: []
+		}
+	}
+}
 
+const Didact = {
+	createElement,
+}
+
+/** @jsx Didact.createElement */
+const element = (
+	<div id='foo'>
+		<a>bar</a>
+		<b />
+	</div>
+)
+
+const container = document.getElementById('root')
+ReactDOM.render(element, container)
 
